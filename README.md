0b24f3d9d675bfbff0b8f038db5af5533f31211e
Miao <jianghong.miao@li.me>
2019-04-13 18:08:09 +0800

    添加目录图片

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ OpenGL
 ## Platforms ##
 * Android 4.3+ SDK (Java)
 
-该仓库代码是对标吞吐量《OPENGL ES 3.0编程指南 原书第2版》中android的java代码。
+该仓库代码是对标吞吐量《OpenGL ES应用开发实践指南  Android卷》、《OPENGL ES 3.0编程指南 原书第2版》两本书中android代码的总结，
 后续会不断往上提交其他学习的代码。
+从效果图上很明显可以看到Opengl功能的强大，跟着代码，开始学习吧！！
 
-
+![login](https://github.com/1531074759/OpenGL/raw/master/screenshots/OpenglES.png)
 ![login](https://github.com/1531074759/OpenGL/raw/master/screenshots/takingthenextstep.gif)
 ![login](https://github.com/1531074759/OpenGL/raw/master/screenshots/lighting.gif)
 ![login](https://github.com/1531074759/OpenGL/raw/master/screenshots/chapters1.png)
--- firstopenglproject/src/main/java/com/firstopenglproject/android/FirstOpenGLProjectRenderer.java
+++ firstopenglproject/src/main/java/com/firstopenglproject/android/FirstOpenGLProjectRenderer.java
@@ -5,18 +5,16 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/kbogla for more book information.
-***/
+ ***/
 package com.firstopenglproject.android;
 
-import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
-import static android.opengl.GLES20.glClear;
-import static android.opengl.GLES20.glClearColor;
-import static android.opengl.GLES20.glViewport;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView.Renderer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import android.opengl.GLSurfaceView.Renderer;
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 
 public class FirstOpenGLProjectRenderer implements Renderer {
     @Override
@@ -24,7 +22,7 @@ public class FirstOpenGLProjectRenderer implements Renderer {
         // Set the background clear color to red. The first component is
         // red, the second is green, the third is blue, and the last
         // component is alpha, which we don't use in this lesson.
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        GLES20.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
     }
 
     /**
@@ -32,16 +30,14 @@ public class FirstOpenGLProjectRenderer implements Renderer {
      * called at least once when the surface is initialized. Keep in mind that
      * Android normally restarts an Activity on rotation, and in that case, the
      * renderer will be destroyed and a new one created.
-     * 
-     * @param width
-     *            The new width, in pixels.
-     * @param height
-     *            The new height, in pixels.
+     *
+     * @param width  The new width, in pixels.
+     * @param height The new height, in pixels.
      */
     @Override
     public void onSurfaceChanged(GL10 glUnused, int width, int height) {
         // Set the OpenGL viewport to fill the entire surface.
-        glViewport(0, 0, width, height);
+        GLES20.glViewport(0, 0, width, height);
     }
 
     /**
@@ -51,6 +47,6 @@ public class FirstOpenGLProjectRenderer implements Renderer {
     @Override
     public void onDrawFrame(GL10 glUnused) {
         // Clear the rendering surface.
-        glClear(GL_COLOR_BUFFER_BIT);
+        GLES20.glClear(GL_COLOR_BUFFER_BIT);
     }
 }
