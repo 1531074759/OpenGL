96f516ee68b65f75886b66bf128e82d8bf0bec15
Aric <1531074759@qq.com>
2020-03-10 00:18:03 +0800

    练习glDrawArrays API的使用

--- learn/src/main/java/com/opengl/learn/GlDrawArraysRender.java
+++ learn/src/main/java/com/opengl/learn/GlDrawArraysRender.java
@@ -87,7 +87,7 @@ public class GlDrawArraysRender implements GLSurfaceView.Renderer {
                     -0.5f, -0.5f, 0.0f,  // v3
                     0.5f, -0.5f, 0.0f,  // v4
                     0.5f, 0.0f, 0.0f,  // v5
-//                    0.0f, 0.5f, 0.0f, // v1
+                    0.0f, 0.5f, 0.0f, // v1
             };
 
     private static final String TAG = GlDrawArraysRender.class.getSimpleName();
