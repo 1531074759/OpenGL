dc47c4d47758d9144306c55c7bd0968b557e9340
Aric <1531074759@qq.com>
2020-03-16 00:01:14 +0800

    练习glActiveTexture API的使用

--- learn/src/main/assets/activetexture_fragmentShader.glsl
+++ learn/src/main/assets/activetexture_fragmentShader.glsl
@@ -9,15 +9,3 @@ void main()
     gl_FragColor = texture2D(uTextureUnit, vTexturePosition);
 //    gl_FragColor *= fColor;
 }
-
-
-
-
-
-//uniform sampler2D u_TextureUnit;
-//varying vec2 v_TextureCoordinates;
-//
-//void main()
-//{
-//    gl_FragColor = texture2D(u_TextureUnit, v_TextureCoordinates);
-//}
